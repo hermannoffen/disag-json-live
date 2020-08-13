2fff8e596e569fcfe177a0e62c631cc14e66d132
René Hoffmann <renehoffmann91@gmail.com>
2020-08-13 22:07:39 +0200

    initialize project "udp-testdata-generator"
    This application is intended to broadcast Disag-JSON-Live data via UDP.

---
+++ src/udp-testdata-generator/Main.pas
@@ -0,0 +1,24 @@
+﻿unit Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TMainForm = class(TForm)
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
